3a21fc691946b647e541d2a9da39f4ad9b3536c5
Yunan Zhang <yunanzhang7715@gmail.com>
2022-10-31 23:44:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,31 @@
+import requests
+import base64
 import streamlit as st
 
-st.title('st.experimental_get_query_params')
+URL = "https://horrible-mole-67.loca.lt"
+headers = {'Bypass-Tunnel-Reminder': "go",
+           'mode': 'no-cors'}
 
-with st.expander('About this app'):
-  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
-
-# 1. Instructions
-st.header('1. Instructions')
-st.markdown('''
-In the above URL bar of your internet browser, append the following:
-`?name=Jack&surname=Beanstalk`
-after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
-such that it becomes 
-`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
-''')
-
-
-# 2. Contents of st.experimental_get_query_params
-st.header('2. Contents of st.experimental_get_query_params')
-st.write(st.experimental_get_query_params())
-
-
-# 3. Retrieving and displaying information from the URL
-st.header('3. Retrieving and displaying information from the URL')
-
-firstname = st.experimental_get_query_params()['firstname'][0]
-surname = st.experimental_get_query_params()['surname'][0]
-
-st.write(f'Hello **{firstname} {surname}**, how are you?')
+def check_if_valid_backend(url):
+    try:
+        resp = requests.get(url, timeout=5, headers=headers)
+        return resp.status_code == 200
+    except requests.exceptions.Timeout:
+        return False
+    
+def call_dalle(url, text, num_images=1):
+    data = {"text": text, "num_images": num_images}
+    resp = requests.post(url + "/dalle", headers=headers, json=data)
+    if resp.status_code == 200:
+        return resp
+    
+def create_and_show_images(text, num_images):
+    valid = check_if_valid_backend(URL)
+    if not valid:
+        st.write("Backend service is not running")
+    else:
+        resp = call_dalle(URL, text, num_images)
+        if resp is not None:
+            for data in resp.json():
+                img_data = base64.b64decode(data)
+                st.image(img_data)
